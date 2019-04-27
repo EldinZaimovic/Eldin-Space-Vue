f37fea1348b879cd3c485a11cba8f88b785bc44a
Eldin Zaimovic <eldin@zaimovic.com>
2019-04-27 18:51:51 +0200

    Add Google Analytics

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import StoryblokVue from 'storyblok-vue'
-
+import VueAnalytics from 'vue-analytics'
 
 
 Vue.config.productionTip = false
+const isProd = process.env.NODE_ENV === "production"
 
 new Vue({
   router,
@@ -14,3 +15,11 @@ new Vue({
 }).$mount('#app')
 
 Vue.use(StoryblokVue)
+Vue.use(VueAnalytics, {
+  id: 'UA-139190314-1',
+  router,
+  debug: {
+    enabled: !isProd,
+    sendHitTask: isProd
+  }
+})
